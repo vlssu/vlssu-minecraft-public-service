bfb714450c107c08ce4aabc3d0d9daa986ee46f4
飒爽师叔 <43847794+vlssu@users.noreply.github.com>
2023-02-15 15:27:32 +0800

    fix

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -73,7 +73,7 @@
                     </a>
                 </li>
                 <li class="nav-item d-none d-lg-block ml-lg-4">
-                    <a href="https://cloud.vlssu.com/#/s/ZNCW" target="_blank" rel="noopener"
+                    <a href="https://cloud.vlssu.com/s/ZNCW" target="_blank" rel="noopener"
                        class="btn btn-neutral btn-icon">
                 <span class="btn-inner--icon">
                   <i class="fa fa-cloud-download mr-2"></i>
