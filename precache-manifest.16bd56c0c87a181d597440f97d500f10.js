64432e53524167e556d3903a60450a6edb511024
vlssu <vlssu@users.noreply.github.com>
2024-08-18 07:45:22 +0000

    deploy: ae3d44cc3184c3578db6826046ce056b008b359f

--- precache-manifest.16bd56c0c87a181d597440f97d500f10.js
+++ precache-manifest.16bd56c0c87a181d597440f97d500f10.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "5782e375c97b276f79f3",
-    "url": "/js/app.3663b58d.js"
+    "revision": "8e550fe6285c12cad53f",
+    "url": "/js/app.ed9314c5.js"
   },
   {
     "revision": "40b69e8799567c6a10d2",
@@ -48,7 +48,7 @@ self.__precacheManifest = [
     "url": "/fonts/nucleo-icons.c1733565.eot"
   },
   {
-    "revision": "f95fb4171b754fd23583a08ff5df5490",
+    "revision": "1248bfdfce982fce74b08aa1fa968e22",
     "url": "/index.html"
   },
   {
@@ -60,16 +60,12 @@ self.__precacheManifest = [
     "url": "/favicon.png"
   },
   {
-    "revision": "c014eadd62065fd0f7864e1796a5fd87",
-    "url": "/favicon.svg"
-  },
-  {
     "revision": "0bab5ae4d8eb33ed096e89d0912170f3",
     "url": "/img/brand/afdian.png"
   },
   {
-    "revision": "4d12e9879ada2aa0444b8da9d9e1f4a1",
-    "url": "/img/brand/banner_1.jpg"
+    "revision": "c014eadd62065fd0f7864e1796a5fd87",
+    "url": "/favicon.svg"
   },
   {
     "revision": "298fb27eae1bfea193134e37baf906fa",
@@ -80,40 +76,36 @@ self.__precacheManifest = [
     "url": "/img/brand/favicon.png"
   },
   {
-    "revision": "e0d24a97423950853b8668eee5cf9c98",
-    "url": "/img/brand/banner_2.jpg"
+    "revision": "cc4638ad35dae35ce68c767e9f482784",
+    "url": "/img/brand/blue.png"
   },
   {
     "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
     "url": "/img/brand/github-white-slim.png"
   },
   {
-    "revision": "d96d8dc80185dbe42742f3884049b0b9",
-    "url": "/img/brand/VLSSUBLOG.svg"
+    "revision": "ace9329cafce4e18f7b10d05a3717823",
+    "url": "/img/brand/head.png"
   },
   {
     "revision": "298fb27eae1bfea193134e37baf906fa",
     "url": "/img/brand/white.png"
   },
   {
-    "revision": "ace9329cafce4e18f7b10d05a3717823",
-    "url": "/img/brand/head.png"
+    "revision": "e0d24a97423950853b8668eee5cf9c98",
+    "url": "/img/brand/banner_2.jpg"
   },
   {
-    "revision": "cc4638ad35dae35ce68c767e9f482784",
-    "url": "/img/brand/blue.png"
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
   },
   {
     "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
     "url": "/img/theme/img-1-1200x1000.jpg"
   },
   {
-    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
-    "url": "/img/theme/img-2-1200x1000.jpg"
-  },
-  {
-    "revision": "594b1ee1d95ada356eaad078e9217932",
-    "url": "/img/ill/ill-2.svg"
+    "revision": "28f64ea06d250f72110f5260faccd663",
+    "url": "/img/brand/banner_3.jpg"
   },
   {
     "revision": "cad5631eacb094a148ea2ba20669c843",
@@ -124,36 +116,44 @@ self.__precacheManifest = [
     "url": "/img/theme/landing.jpg"
   },
   {
-    "revision": "c48561e6d0a36ae4dbf3493aeeaf3724",
-    "url": "/img/theme/team-1-800x800.jpg"
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
   },
   {
     "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
     "url": "/img/theme/profile.jpg"
   },
   {
-    "revision": "80b8772aaa8f07c9b535f9206ead0d36",
-    "url": "/img/theme/team-3-800x800.jpg"
-  },
-  {
     "revision": "bcb489b46803d86cc679f072b418e430",
     "url": "/img/theme/promo-1.png"
+  },
+  {
+    "revision": "4d12e9879ada2aa0444b8da9d9e1f4a1",
+    "url": "/img/brand/banner_1.jpg"
+  },
+  {
+    "revision": "d96d8dc80185dbe42742f3884049b0b9",
+    "url": "/img/brand/VLSSUBLOG.svg"
   },
   {
     "revision": "065926b182a2fcf67c61cdd9fe166326",
     "url": "/img/theme/team-2-800x800.jpg"
   },
   {
+    "revision": "80b8772aaa8f07c9b535f9206ead0d36",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
     "revision": "33d5be733c74bfd41e5eb3e8d45ea26f",
     "url": "/img/theme/team-4-800x800.jpg"
   },
   {
-    "revision": "28f64ea06d250f72110f5260faccd663",
-    "url": "/img/brand/banner_3.jpg"
-  },
-  {
     "revision": "3ad1a3f5c1f402200fa75a61118a4bf9",
     "url": "/img/theme/team-5-800x800.jpg"
+  },
+  {
+    "revision": "c48561e6d0a36ae4dbf3493aeeaf3724",
+    "url": "/img/theme/team-1-800x800.jpg"
   },
   {
     "revision": "96b14dd47592daf8c3afff354dd81f21",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.59a03793e2b2daa469373f396a957ba3.js"
+  "/precache-manifest.16bd56c0c87a181d597440f97d500f10.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vlssu-minecraft-public-service"});
